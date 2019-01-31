a10082745f1bb52f90a8f0872ba07cd8408441da
Marco De La Pierre <marco.delapierre@gmail.com>
2019-01-31 09:26:21 +0000

    Marco: changes for GS-10984

--- install_lammps.sh
+++ install_lammps.sh
@@ -7,6 +7,15 @@ make lammps
 cd /root/lammps*/src/
 cp -r $deepmd_source_dir/source/build/USER-DEEPMD .
 
+#Patch to enable OpenMP compile
+sed -e '/CCFLAGS.*=/ s/$/ -fopenmp/g' -e '/LINKFLAGS.*=/ s/$/ -fopenmp/g' MAKE/Makefile.serial >MAKE/Makefile.omp
+
 #Now build Lammps
+make yes-manybody
+make yes-mc
+make yes-rigid
+make yes-user-meamc
 make yes-user-deepmd
-make serial -j20
+
+make mpi-stubs
+make omp -j4
