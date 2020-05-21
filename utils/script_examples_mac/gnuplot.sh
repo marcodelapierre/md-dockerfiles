49466c24d0c9fc1084430df3cbb9fc550b56c56e
Marco De La Pierre <marco.delapierre@gmail.com>
2020-05-21 06:38:41 +0000

    updated example script for gnuplot / octave

--- utils/script_examples_mac/gnuplot.sh
+++ utils/script_examples_mac/gnuplot.sh
@@ -25,4 +25,4 @@ docker run --rm $flag \
 	-v $(echo ~):$(echo ~) -w $(pwd) \
 	-u $(id -u):$(id -g) \
 	-e DISPLAY=docker.for.mac.localhost:0 -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH \
-	marcodelapierre/octave_gnuplot:4.2.2_5.2.2_2 $cmd $args
+	marcodelapierre/octave_gnuplot:4.2.2_5.2.2_4 $cmd $args
